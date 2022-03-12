d2902ef552559b24d6fd1ffb1c16bb9f7a0ecc9f
Dmitry <Dimabroshkov@gmail.com>
2022-03-12 12:19:58 +0200

    Add draw tree function

--- 01-node-and-npm/src/index.js
+++ 01-node-and-npm/src/index.js
@@ -1,1 +1,27 @@
-const three = require('../fixtures.json')
+const tree = require('../fixtures.json')
+
+function drawTree(tree, index = 1, items = null, level = 1) {
+    let indent = ' '
+    let prefix = ''
+    let postfix = ''
+
+    if (level === 2) {
+        const isLastNode = index === items.length - 1
+        prefix = isLastNode ? '└── ' : '├── '
+    }
+    if (level > 2) {
+        indent = ' '.repeat(level * 4 - 8)
+        prefix = '|'
+        postfix = '└── '
+    }
+
+    console.log('\n', prefix + indent + postfix + tree.name)
+
+    if ('items' in tree) {
+        tree.items.forEach((item, index, array) => {
+            drawTree(item, index, array, level + 1)
+        })
+    }
+}
+
+drawTree(tree)
