1a46c6cd797cd577ec020100ec51e91869423222
Dmitry <Dimabroshkov@gmail.com>
2022-03-25 20:21:43 +0200

    Clean up

--- 01-node-and-npm/index.js
+++
@@ -1,4 +0,0 @@
-const wrapper = require('./src')
-const tree = require('./fixtures.json')
-
-wrapper.drawTree(tree)
--- 04-streams/src/utils/random_int_generator.js
+++
@@ -1,7 +0,0 @@
-function* randomNumberGenerator(maxNumber, separator) {
-    while (true) {
-        yield Math.ceil(Math.random() * maxNumber).toString() + separator
-    }
-}
-
-module.exports = randomNumberGenerator
