9b660e1f4d0464af0eeaf89655375305c4020362
Dmitry <Dimabroshkov@gmail.com>
2022-04-23 13:50:43 +0300

    Add Socket logic

--- 12-websocket/src/index.js
+++ 12-websocket/src/index.js
@@ -1,16 +1,41 @@
 // TODO
-// Ask for location - https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API
+// Add Pin on Maps and centering in the new locations
+// Draw a L.polygon from array from file
 // Enable Push API - https://developer.mozilla.org/en-US/docs/Web/API/Push_API
 // Setup websocket and input filed from index html
 // After adding new location to JSON file send push notification
 
 
 const express = require('express')
+const fs = require('fs')
 const path = require('path')
+const { Server } = require('socket.io')
 
 const app = express()
+const server = app.listen(3000, () => console.log('Hello from 3000'))
+
+const io = new Server(server)
+const locationStoragePath = './assets/locations.json'
+
+const writeToFile = (content) => {
+    try {
+        fs.appendFileSync(path.join(__dirname, locationStoragePath), JSON.stringify(content) + ',')
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+io.on('connection', (socket) => {
+    socket.on('user location', (location) => {
+        console.log('location: ', location)
+        writeToFile(location)
+
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+})
+
 
 app.use('/', express.static(path.join(__dirname, 'assets')))
-
-
-app.listen(3000, () => console.log('Hello from 3000'))
